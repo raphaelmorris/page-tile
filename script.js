edb2d66d39c8fa83af66259977387c120065e9f9
Raphael Morris <raphaelmorris@MacBook-Pro-de-Raphael.local>
2022-05-12 17:30:11 +0200

    avancée sur la transition

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var $loader = document.querySelector('.loader')
+var $popup = document.querySelector('.toto')
 
 window.onload = function() {
   $loader.classList.remove('loader--active')
@@ -8,6 +9,8 @@ document.querySelector('.btn').addEventListener('click', function () {
   $loader.classList.add('loader--active')
   
   window.setTimeout(function () {
-    $loader.classList.remove('loader--active')
-  }, 1000)
+    //$loader.classList.remove('loader--active'),
+    window.location = 'page2.html';
+  }, 600)
+
 })
